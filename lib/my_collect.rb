d40d3049061c1e99cf52c77a41dd80dde22e65a2
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-12 01:54:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ require 'pry'
 def my_collect (collect_arg)
 update_arg = []
 counter = 0
-  while counter <= coollect_arg.size
+  while counter <= collect_arg.size
    item = collect_arg[counter]
    yield(item)
    binding.pry
